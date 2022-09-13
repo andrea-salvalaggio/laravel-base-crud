f355845fb7020a85f547cddea680548244b0e8bc
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-13 16:16:24 +0200

    Feat: visualize list of comics in home

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicController extends Controller
 {
     //
+    public function index(){
+        $comics = Comic::all();
+        return view('home', compact('comics'));
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,34 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                Main
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Title</th>
+                          <th>Series</th>
+                          <th>Type</th>
+                          <th>Sale Date</th>
+                          <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @forelse ($comics as $comic)
+                            <tr>
+                                <td> {{ $comic->id }} </td>
+                                <td> {{ $comic->title }} </td>
+                                <td> {{ $comic->series }} </td>
+                                <td> {{ $comic->type }} </td>
+                                <td> {{ $comic->sale_date }} </td>
+                                <td> {{ $comic->price }} </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="10"> Comics are not available  </td>
+                            </tr>
+                        @endforelse
+                    </tbody>                
+                </table>
             </div>
         </div>
     </div>
