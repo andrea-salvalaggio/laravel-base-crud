8f483f618bb453d1eaec8a3c1e2d8330ad520979
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-13 14:59:39 +0200

    Add: create structure for main and home file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                Main
+            </div>
+        </div>
+    </div>
+@endsection
