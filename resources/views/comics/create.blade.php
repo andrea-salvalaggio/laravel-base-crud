c0c7eb979a71e3eac3b1a6632ada0b6aaf1494f1
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-14 14:35:22 +0200

    Add: create.blade.php

---
+++ resources/views/comics/create.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Comics: ' . $comic->title)
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
+@endsection
