a06dc82567ff6ac1a738b6f562712854718a8a22
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-14 14:48:09 +0200

    Feat: add a form into create.blade

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,12 +1,30 @@
 @extends('layouts.main')
 
-@section('title', 'Comics: ' . $comic->title)
+@section('title', 'Create a new comic')
 
 @section('main-content')
     <div class="container">
         <div class="row">
             <div class="col-12">
-                
+                <form action="{{ route('comics.store') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                            placeholder="Enter email">
+                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                            else.</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index')->name('admin.home');
-Route::get('/comics', 'ComicController@index')->name('comics.index');
-Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
+// Route::get('/comics', 'ComicController@index')->name('comics.index');
+// Route::get('/comics/{comic}', 'ComicController@show')->name('comics.show');
+Route::resource('/comics', 'ComicController');
