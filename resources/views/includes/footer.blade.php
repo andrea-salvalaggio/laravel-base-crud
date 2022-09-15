3c2cc1ca0a54ebbce4b5ed6f9cf92f324783672a
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-15 20:42:44 +0200

    Style: add style to the footer

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,3 +1,8 @@
 <footer>
-    Footer
+    <nav class="navbar navbar-expand-lg bg-dark">
+        <div class="container-fluid justify-content-center">
+            <a class="navbar-brand text-white fs-6" href="{{ route('admin.home') }}">Copyright © 2022 Boolmics</a>
+            </div>
+        </div>
+    </nav>
 </footer>
