64410e390a1f0c8c24d0f9c5a3aa327ee589e3d1
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-09-16 17:04:32 +0200

    Add: 404 custom page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', '404 | Not Found')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center my-5">
+                <img  class="img-fluid" src="https://media.makeameme.org/created/404-or-page-f6738564cf.jpg" alt="404 Not Found">
+            </div>
+        </div>
+    </div>
+@endsection
